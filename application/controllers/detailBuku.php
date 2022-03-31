b6385e67d3be38ad570d854e4b1436e4f89e0542
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2022-03-31 18:24:47 +0700

    pertemuan ke 3

---
+++ application/controllers/detailBuku.php
@@ -0,0 +1,31 @@
+<?php
+
+class detailBuku extends CI_Controller
+{
+
+    public function detailBuku()
+    {
+        $id = $this->uri->segment(3);
+        $buku = $this->ModelBuku->joinKategoriBuku(['buku.id' => $id])->result();
+
+        $data['user'] = "Pengunjung";
+        $data['title'] = "Detail Buku";
+
+        foreach ($buku as $fields) {
+            $data['judul'] = $fields->judul_buku;
+            $data['pengarang'] = $fields->pengarang;
+            $data['penerbit'] = $fields->penerbit;
+            $data['kategori'] = $fields->kategori;
+            $data['tahun'] = $fields->tahun;
+            $data['isbn'] = $fields->isbn;
+            $data['gambar'] = $fields->gambar;
+            $data['dipinjam'] = $fields->dipinjam;
+            $data['dibooking'] = $fields->dibooking;
+            $data['stok'] = $fields->stok;
+            $data['id'] = $fields->id;
+        }
+        $this->load->view('templates/templates-user/header', $data);
+        $this->load->view('buku/detail-buku', $data);
+        $this->load->view('templates/templates-user/footer');
+    }
+}
--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -3,27 +3,43 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pustaka-Booking</title>
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/user/css/bootstrap.css">
+    <title>Pustaka-Booking | <?= $judul; ?></title>
+    <link rel="icon" href="<?= base_url('assets/img/logo'); ?> logo-pb.png" type="image/png">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?> user/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?> vendor/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?> datatable/datatables.css">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="<?= base_url(); ?>">EASIS</a>
+            <a href="<?= base_url(); ?>" class="navbar-brand">Pustaka</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="<?= base_url(); ?>">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="<?= base_url(); ?>mahasiswa">Mahasiswa</a>
-                    <a class="nav-item nav-link" href="<?= base_url(); ?>about">About</a>
-                    <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    <a href="<?= base_url(); ?>" class="nav-item nav-link active">Beranda <span class="sr-only">(Current)</span></a>
+
+                    <?php
+                    if (!empty($this->session->userdata("email"))) { ?>
+                        <a href="#" class="nav-item nav-link">Booking Buku</a>
+                        <a href="<?= base_url('member/myprofil') ?>" class="nav-item nav-link">Profil Saya</a>
+                        <a href="<?= base_url('member/logout') ?>" class="nav-item nav-link">Log Out</a>
+
+                    <?php } else { ?>
+
+                        <a href="#" class="nav-item nav-link" data-toggle="modal" data-target="#daftarModal"><i class="fas fw fa-login"></i> Daftar </a>
+                        <a href="#" class="nav-item nav-link" data-toggle="modal" data-target="#loginModal"><i class="fas fw fa-login"></i> Login </a>
+                    <?php } ?>
+                    <span class="nav-item nav-link nav-right" style="display:block; margin-left:20px">Selamat Datang <b><?= $user; ?></b></span>
                 </div>
             </div>
         </div>
     </nav>
-    <div class="container mt-5">
+    <div class="container">
+
+</html>
